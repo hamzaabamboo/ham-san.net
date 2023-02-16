9981d844478f14553edc3faa4265d5086fbd0660
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-02-16 18:46:11 +0700

    chore: remove log

--- client/src/routes/projects/[slug]/+page.server.ts
+++ client/src/routes/projects/[slug]/+page.server.ts
@@ -5,11 +5,10 @@ import { GetProjectBySlugDocument } from '../../../graphql/generated/server';
 
 /** @type {import('./$types').PageServerLoad} */
 export const load: Load = async ({ params }) => {
-	const data = await graphQLClient.request(GetProjectBySlugDocument,{
+	const data = await graphQLClient.request(GetProjectBySlugDocument, {
 		slug: params.slug
 	});
 
-	console.log('FUCK', params.slug, data?.projects?.data[0]?.attributes?.title);
 	if (data) {
 		return data;
 	}
