579f63f891e8d10de3e7cf32f50c3df5c4e3f379
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-02-17 17:43:49 +0700

    fix: missing /graphql

--- client/src/graphql/index.ts
+++ client/src/graphql/index.ts
@@ -1,4 +1,4 @@
 import { PUBLIC_API_URL } from '$env/static/public';
 import { GraphQLClient } from 'graphql-request';
 
-export const graphQLClient = new GraphQLClient(PUBLIC_API_URL);
+export const graphQLClient = new GraphQLClient(PUBLIC_API_URL + '/graphql');
