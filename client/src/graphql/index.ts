ee5dfdfc8bd53f3ab1be2d1ece8d655ab58a0e90
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-02-17 17:29:27 +0700

    fix: API URL

--- client/src/graphql/index.ts
+++ client/src/graphql/index.ts
@@ -1,3 +1,4 @@
+import { PUBLIC_API_URL } from '$env/static/public';
 import { GraphQLClient } from 'graphql-request';
 
-export const graphQLClient = new GraphQLClient('http://127.0.0.1:1337/graphql');
+export const graphQLClient = new GraphQLClient(PUBLIC_API_URL);
