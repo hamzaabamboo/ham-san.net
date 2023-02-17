bf0fb270e5b35a09a1da1798c6ee7c95cdc0bbb0
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-02-17 16:14:11 +0700

    fix: wrong key

--- client/src/utils/domain.ts
+++ client/src/utils/domain.ts
@@ -1,3 +1,3 @@
-import { PUBLIC_API_KEY } from '$env/static/public';
+import { PUBLIC_API_URL } from '$env/static/public';
 
-export const API_URL = PUBLIC_API_KEY ?? 'http://127.0.0.1:1337';
+export const API_URL = PUBLIC_API_URL ?? 'http://127.0.0.1:1337';
