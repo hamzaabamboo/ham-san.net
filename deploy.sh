653012d6859014a299c3140ed8a4d726b18359a6
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-02-22 02:02:15 +0700

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 yes | docker system prune
-docker-compose build
-docker-compose up --no-deps -d --build
+docker-compose up --no-deps -d --build backend
 docker-compose restart backend
+docker-compose up --no-deps -d --build client
+docker-compose restart client
