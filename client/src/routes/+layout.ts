41f6ec399cf5552752339d14fed1479ab798f425
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-02-23 01:10:31 +0700

    fix: language flashing again

--- client/src/routes/+layout.ts
+++ client/src/routes/+layout.ts
@@ -1,6 +1,7 @@
 import { locale, loadTranslations } from '@i18n';
 import type { LayoutLoadEvent } from './$types';
 import { LANGUAGES } from '@utils/localization';
+import { browser } from '$app/environment';
 
 /** @type {import('./$types').LayoutLoad} */
 export const load = async ({ url, data }: LayoutLoadEvent) => {
@@ -8,8 +9,8 @@ export const load = async ({ url, data }: LayoutLoadEvent) => {
 	const { preferredLanguages = [], currentLanguage } = data;
 	const defaultLocale =
 		preferredLanguages.find((lang) => !!lang && LANGUAGES.includes(lang)) || 'en'; // get from cookie, user session, ...
-
-	const initLocale = locale.get() || currentLanguage || defaultLocale; // set default if no locale already set
+	const serverLanguage = currentLanguage || defaultLocale;
+	const initLocale = browser ? locale.get() ?? serverLanguage : serverLanguage; // set default if no locale already set
 
 	await loadTranslations(initLocale, pathname); // keep this just before the `return`
 
