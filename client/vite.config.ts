9344b30796748d7db8f8c09b0d7f38d97e58eadf
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-02-25 03:20:50 +0700

    fix: remove pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from 'vite-imagetools';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import type { UserConfig } from 'vite';
 
 const supportedExtensions = ['png', 'jpg', 'jpeg'];
@@ -14,15 +13,15 @@ const config: UserConfig = {
 				const extension = url.pathname.substring(url.pathname.lastIndexOf('.') + 1) as string;
 				if (supportedExtensions.includes(extension)) {
 					return new URLSearchParams({
-						format: 'avif;webp;' + extension,
+						format: 'webp;' + extension,
 						picture: 'true',
 						width: '1980'
 					});
 				}
 				return new URLSearchParams();
 			}
-		}),
-		SvelteKitPWA()
+		})
+		// SvelteKitPWA()
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
