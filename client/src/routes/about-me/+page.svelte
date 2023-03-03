37c355f862c48fbbc2b726c6230c35d511b26e33
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-03-03 22:57:57 +0700

    feat: preload about me

--- client/src/routes/about-me/+page.svelte
+++ client/src/routes/about-me/+page.svelte
@@ -12,14 +12,17 @@
 	import aboutMeBanner from '@assets/about-me-banner.jpg';
 	import { formatMonthYear } from '@utils/date';
 	import TagItem from '@components/tags/TagItem.svelte';
+	import type { PageServerData } from './$types';
 
-	$: query = fetchAboutMe({ variables: { locale: $locale } });
+	export let data: PageServerData;
 
-	$: experiences = $query.data.experiences?.data;
-	$: educations = $query.data.educations?.data;
-	$: aboutMetadata = $query.data.aboutMe?.data?.attributes;
+	$: query = data.data;
+
+	$: experiences = query.experiences?.data;
+	$: educations = query.educations?.data;
+	$: aboutMetadata = query.aboutMe?.data?.attributes;
 	$: skills = groupBy(
-		$query.data.tags?.data?.filter((tag) => tag.attributes?.type !== Enum_Tag_Type.NonDev),
+		query.tags?.data?.filter((tag) => tag.attributes?.type !== Enum_Tag_Type.NonDev),
 		(tag) => tag.attributes?.type
 	);
 
@@ -30,83 +33,79 @@
 	<title>{$t('common.about-me')} | {$t('common.name')}</title>
 </svelte:head>
 
-{#if $query.loading}
-	<Container class="text-center pt-8">Loading</Container>
-{:else}
-	<Container class="pt-12 flex flex-row  flex-wrap">
-		<div class="px-4 flex flex-col w-full md:w-3/5">
-			<div class="mb-2">
-				<Typography variant="h1">{$t('about-me.title')}</Typography>
-			</div>
-			{#if !!aboutMetadata}<MarkdownRenderer content={aboutMetadata.introduction} />{/if}
-			<Typography variant="h2">{$t('about-me.experiences')}</Typography>
-			{#if experiences && experiences?.length > 0}
-				{#each experiences as experience}
-					<div class="flex flex-col mb-2">
-						<Typography variant="h4" class="mb-1">{experience.attributes?.title}</Typography>
-						<div class="flex flex-row mb-2">
-							<Typography variant="subtitle" class="mr-2"
-								>{[
-									experience.attributes?.position,
-									experience.attributes?.description,
-									`${formatDate(experience.attributes?.start)} - ${
-										formatDate(experience.attributes?.end) ?? $t('common.present')
-									}`
-								]
-									.filter((i) => !!i)
-									.join(' | ')}</Typography
-							>
-						</div>
-						{#if experience.attributes?.content}
-							<MarkdownRenderer content={experience.attributes?.content} />
-						{/if}
-					</div>
-				{/each}
-			{/if}
-			<hr class="mb-2" />
-			<Typography variant="h2">{$t('about-me.education')}</Typography>
-			{#if educations && educations?.length > 0}
-				{#each educations as education}
-					<div class="mb-2 flex flex-col">
-						<Typography variant="h4" class="mb-1">{education.attributes?.title}</Typography>
-						<div class="flex flex-row mb-2">
-							<Typography variant="subtitle" class="mr-2"
-								>{[
-									education.attributes?.description,
-									`${formatDate(education.attributes?.start)} - ${
-										formatDate(education.attributes?.end) ?? $t('common.present')
-									}`
-								]
-									.filter((i) => !!i)
-									.join(' | ')}</Typography
-							>
-						</div>
-						{#if education.attributes?.content}
-							<MarkdownRenderer content={education.attributes?.content} />
-						{/if}
-					</div>
-				{/each}
-			{/if}
-			<hr class="mb-2" />
-			<Typography variant="h2">{$t('about-me.skills')}</Typography>
-			<div class="flex flex-col">
-				{#each Object.keys(skills) as category}
-					<div class="flex flex-row flex-wrap mb-2">
-						<Typography variant="h6" class="mr-2"
-							>{$t(`common.${toKebabCase(category)}`)} :</Typography
-						>
-						{#each skills[category] as tag}
-							{#if tag.attributes} <TagItem tag={tag?.attributes} class="mr-2" /> {/if}
-						{/each}
-					</div>
-				{/each}
-			</div>
-			<hr />
+<Container class="pt-12 flex flex-row  flex-wrap">
+	<div class="px-4 flex flex-col w-full md:w-3/5">
+		<div class="mb-2">
+			<Typography variant="h1">{$t('about-me.title')}</Typography>
 		</div>
-		<div
-			class="bg-cover bg-left w-full md:w-2/5"
-			style:height="400px"
-			style:background-image="url('{aboutMeBanner.fallback.src}')"
-		/>
-	</Container>
-{/if}
+		{#if !!aboutMetadata}<MarkdownRenderer content={aboutMetadata.introduction} />{/if}
+		<Typography variant="h2">{$t('about-me.experiences')}</Typography>
+		{#if experiences && experiences?.length > 0}
+			{#each experiences as experience}
+				<div class="flex flex-col mb-2">
+					<Typography variant="h4" class="mb-1">{experience.attributes?.title}</Typography>
+					<div class="flex flex-row mb-2">
+						<Typography variant="subtitle" class="mr-2"
+							>{[
+								experience.attributes?.position,
+								experience.attributes?.description,
+								`${formatDate(experience.attributes?.start)} - ${
+									formatDate(experience.attributes?.end) ?? $t('common.present')
+								}`
+							]
+								.filter((i) => !!i)
+								.join(' | ')}</Typography
+						>
+					</div>
+					{#if experience.attributes?.content}
+						<MarkdownRenderer content={experience.attributes?.content} />
+					{/if}
+				</div>
+			{/each}
+		{/if}
+		<hr class="mb-2" />
+		<Typography variant="h2">{$t('about-me.education')}</Typography>
+		{#if educations && educations?.length > 0}
+			{#each educations as education}
+				<div class="mb-2 flex flex-col">
+					<Typography variant="h4" class="mb-1">{education.attributes?.title}</Typography>
+					<div class="flex flex-row mb-2">
+						<Typography variant="subtitle" class="mr-2"
+							>{[
+								education.attributes?.description,
+								`${formatDate(education.attributes?.start)} - ${
+									formatDate(education.attributes?.end) ?? $t('common.present')
+								}`
+							]
+								.filter((i) => !!i)
+								.join(' | ')}</Typography
+						>
+					</div>
+					{#if education.attributes?.content}
+						<MarkdownRenderer content={education.attributes?.content} />
+					{/if}
+				</div>
+			{/each}
+		{/if}
+		<hr class="mb-2" />
+		<Typography variant="h2">{$t('about-me.skills')}</Typography>
+		<div class="flex flex-col">
+			{#each Object.keys(skills) as category}
+				<div class="flex flex-row flex-wrap mb-2">
+					<Typography variant="h6" class="mr-2"
+						>{$t(`common.${toKebabCase(category)}`)} :</Typography
+					>
+					{#each skills[category] as tag}
+						{#if tag.attributes} <TagItem tag={tag?.attributes} class="mr-2 mb-2" /> {/if}
+					{/each}
+				</div>
+			{/each}
+		</div>
+		<hr />
+	</div>
+	<div
+		class="bg-cover bg-left w-full md:w-2/5"
+		style:height="400px"
+		style:background-image="url('{aboutMeBanner.fallback.src}')"
+	/>
+</Container>
