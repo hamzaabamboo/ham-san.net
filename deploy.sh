f485aaf13d9caf6e99d2d8c49248d97de4859680
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-03-03 23:32:47 +0700

    feat: add delay before building client

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 yes | docker system prune
 docker-compose up --no-deps -d --build backend
 docker-compose restart backend
+sleep 5
 docker-compose up --no-deps -d --build client
 docker-compose restart client
