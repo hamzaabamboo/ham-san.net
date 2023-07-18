667744426a0d34b0f97f423da2cb2901dddcb11e
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-07-19 02:20:47 +0700

    fix: regex gomen

--- client/src/utils/article.ts
+++ client/src/utils/article.ts
@@ -4,7 +4,7 @@ export const cleanArticleContent = (content?: string) => {
     return content?.replaceAll('\\\n', '')?.replaceAll("\\n", "\n\n")
 }
 export const getArticleBanner = (content?: string) => {
-    return /!\[(.*)\]\((.+)".*?"\)/.exec(content ?? '')?.[2].replaceAll('\\', '') ?? undefined
+    return /!\[(.*)\]\((.+?)(?:".*?")?\)/.exec(content ?? '')?.[2].replaceAll('\\', '').trim() ?? undefined
 }
 export const getArticleDescription = (content?: string) => {
     return removeMarkdown(content?.replaceAll(/!\[(.*)\]\((.+)\)/g, "") ?? '')?.replaceAll(/\s+/g, " ").trim().substring(0,100) + '...';
