315e2504f5fbb7b711089109e8e45b4abd8fbc7f
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-07-19 12:59:32 +0700

    fix: page transitions

--- client/src/components/Header.svelte
+++ client/src/components/Header.svelte
@@ -35,48 +35,48 @@
 
 <!-- bind:this={navbar} -->
 <nav
-	class="w-full h-12 flex items-center justify-between px-4 sticky top-0 bg-primary shadow-sm z-20"
-	style="view-transition-name: header"
+	class="w-full h-12 flex items-center justify-between sticky bg-primary top-0 shadow-sm z-20"
 >
-	<!-- Logo Section -->
-	<div class="w-full md:w-auto relative z-20">
-		<!-- <Logo /> -->
-		<a href="/">{$t('common.name')}</a>
-	</div>
-
-	<!-- Desktop Menu-->
-	<div class="items-center hidden md:flex">
-		<div>
-			{#each links as link}
-				<a href={link.href} class="p-2">{link.label}</a>
-			{/each}
+	<div class="w-full h-12 flex items-center justify-between px-4 z-20" style:view-transition-name="header">
+				<!-- Logo Section -->
+		<div class="w-full md:w-auto relative z-20">
+			<!-- <Logo /> -->
+			<a href="/">{$t('common.name')}</a>
 		</div>
-		<div class="px-2">|</div>
-		<div>
-			{#each LANGUAGES as language}
-				{#if $localizationUrls[language]}
-					<a
-						class="px-1"
-						href={$localizationUrls[language]}
-						on:click={() => handleChangeLanguage(language)}
-						>{language.toUpperCase()}
-					</a>
-				{:else}
-					<button class="px-1" on:click={() => handleChangeLanguage(language)}>
-						{language.toUpperCase()}</button
-					>
-				{/if}
-			{/each}
-		</div>
-	</div>
 
-	<!-- Mobile Menu-->
-	<div
-		class="items-center flex relative z-20 md:hidden"
-		on:click={() => (menuOpen = !menuOpen)}
-		style="view-transition-name: header"
-	>
-		<Fa icon={faBars} size="md" />
+		<!-- Desktop Menu-->
+		<div class="items-center hidden md:flex">
+			<div>
+				{#each links as link}
+					<a href={link.href} class="p-2">{link.label}</a>
+				{/each}
+			</div>
+			<div class="px-2">|</div>
+			<div>
+				{#each LANGUAGES as language}
+					{#if $localizationUrls[language]}
+						<a
+							class="px-1"
+							href={$localizationUrls[language]}
+							on:click={() => handleChangeLanguage(language)}
+							>{language.toUpperCase()}
+						</a>
+					{:else}
+						<button class="px-1" on:click={() => handleChangeLanguage(language)}>
+							{language.toUpperCase()}</button
+						>
+					{/if}
+				{/each}
+			</div>
+		</div>
+
+		<!-- Mobile Menu-->
+		<div
+			class="items-center flex relative z-20 md:hidden"
+			on:click={() => (menuOpen = !menuOpen)}
+		>
+			<Fa icon={faBars} size="md" />
+		</div>
 	</div>
 	<div
 		class="fixed w-full h-full z-10 transition-all bg-white top-0 pt-16 md:hidden"
