f389ae780bc1ca628d111c342bc5e76c3b63e268
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2023-09-24 18:17:07 +0900

    feat: change database to postgres

--- api/config/database.ts
+++ api/config/database.ts
@@ -1,14 +1,20 @@
-import path from 'path';
-
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', 'data/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "database"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
+    debug: false,
     useNullAsDefault: true,
   },
   settings: {
-    forceMigrations: false
-  }
+    forceMigrations: false,
+  },
 });
