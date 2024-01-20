7af2ef682140422f89fb4bc858f7871137a21eb9
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2024-01-21 02:43:36 +0900

    feat: namecard, bump version

---
+++ apps/astro/panda.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from '@pandacss/dev';
+import { createPreset } from '@park-ui/panda-preset';
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  presets: [
+    '@pandacss/preset-base',
+    createPreset({
+      accentColor: 'cyan',
+      grayColor: 'neutral',
+      borderRadius: 'sm'
+    })
+  ],
+
+  // Where to look for your css declarations
+  include: ['./src/**/*.{js,jsx,ts,tsx,astro}', './pages/**/*.{js,jsx,ts,tsx,astro}'],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {}
+  },
+
+  jsxFramework: 'react',
+
+  // The output directory for your css system
+  outdir: 'styled-system',
+
+  emitPackage: true
+});
