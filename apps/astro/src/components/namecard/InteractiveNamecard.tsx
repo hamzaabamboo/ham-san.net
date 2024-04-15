a9d2135087a05ffca6797b009f3d7e5b39bbc397
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2024-04-15 20:12:02 +0900

    fix: view transitions breaking stuff for some reason

--- apps/astro/src/components/namecard/InteractiveNamecard.tsx
+++ apps/astro/src/components/namecard/InteractiveNamecard.tsx
@@ -15,45 +15,65 @@ export const InteractiveNamecard = ({ data }: { data: NamecardData }) => {
         ['--color' as 'color']: data.color
       }}
       gap="0"
+      w="full"
       width="var(--width)"
+      h="full"
       height="var(--height)"
-      perspective="calc(2 * var(--width))"
+      perspective="var(--width)"
       perspectiveOrigin="center"
     >
       <Box
-        style={{
-          viewTransitionName: `namecard-${data.variant}-front`
-        }}
+        className="card"
+        position="relative"
         shadow="md"
+        transition="transform"
         animation={{ base: '5s linear infinite spin', _groupHover: 'none' }}
+        transitionTimingFunction="linear"
+        transitionDelay="0.1s"
+        transitionDuration="1s"
         transformStyle="preserve-3d"
       >
-        <Box position="absolute">
+        <Box
+          style={{
+            viewTransitionName: `namecard-${data.variant}-front`
+          }}
+          backfaceVisibility="hidden"
+          position="absolute"
+          top="0"
+          left="0"
+          transform="rotate3d(0,0,0,0deg)"
+        >
           <StyledNamecard data={data} side="front" />
         </Box>
         <Box
           position="absolute"
+          top="0"
+          left="0"
           transformOrigin="left"
           width="var(--thickness)"
           height="var(--height)"
           backgroundColor="var(--color)"
-          transform="rotateY(90deg)"
+          transform="rotate3d(0,1,0,90deg)"
         />
         <Box
           backfaceVisibility="hidden"
           position="absolute"
-          transform="rotateY(180deg) translateZ(var(--thickness))"
+          top="0"
+          left="0"
+          transition="transform"
+          transform="rotate3d(0,1,0,180deg) translate3d(0,0,var(--thickness))"
         >
           <StyledNamecard data={data} side="back" />
         </Box>
         <Box
           position="absolute"
+          top="0"
           right="0"
           transformOrigin="right"
           width="var(--thickness)"
           height="var(--height)"
           backgroundColor="white"
-          transform="rotateY(-90deg)"
+          transform="rotate3d(0,1,0,-90deg)"
         />
       </Box>
     </Stack>
