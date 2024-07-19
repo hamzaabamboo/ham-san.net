aa744a52e3f9f902076cdaa8822b575ef1f8f4db
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2024-07-19 15:24:37 +0900

    feat: prune after build

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,3 +1,4 @@
 docker-compose pull api
 docker-compose down api
 docker-compose up -d api
+yes | docker system prune
--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,3 +1,4 @@
 docker-compose pull client
 docker-compose down client
 docker-compose up -d client
+yes | docker system prune
